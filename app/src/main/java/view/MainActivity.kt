41c6b7cbf47ff47d72386dcd5c7d0f81c03ae226
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2023-11-09 00:07:46 -0500

    añadida funcion on/off boton de musica, sin funcionalidad total

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -2,16 +2,51 @@ package view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.bottlegame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Thread.sleep(5000)
         screenSplash.setKeepOnScreenCondition{false}
+
+        //Listeners y
+        val botonGirar: ImageButton = findViewById(R.id.botonGirar)
+        val botonMusica: ToggleButton = findViewById(R.id.musica)
+        val contador: TextView = findViewById(R.id.cuenta)
+
+
+        botonGirar.setOnClickListener{
+            botonGirar.isEnabled=false
+            contador.visibility = View.VISIBLE
+            empezarContador(contador)
+        }
+        //Logica botón de volumen
+        botonMusica.setOnClickListener{
+            if(botonMusica.isChecked){
+                botonMusica.setCompoundDrawablesWithIntrinsicBounds(null, resources.getDrawable(R.drawable.volume_off_fill0_wght400_grad0_opsz24), null, null)
+                //Parar musica aquí
+            }else{
+                botonMusica.setCompoundDrawablesWithIntrinsicBounds(null, resources.getDrawable(R.drawable.volume_up_fill0_wght400_grad0_opsz24), null, null)
+                //Continuar musica aqui
+            }
+
+        }
+
+
+    }
+
+    private fun empezarContador(contador: TextView){
+
     }
 }
