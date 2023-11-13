97a8da897b6e49b3e4f2c71a6e5b3b66eea2071c
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2023-11-13 13:24:49 -0500

    comentarios mas claros en el codigo realizado

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -30,11 +30,12 @@ class MainActivity : AppCompatActivity() {
         val musicaFondo = MediaPlayer.create(this, R.raw.portal_radio_loop)
         musicaFondo.start()
 
+        //Listener boton de girar
         botonGirar.setOnClickListener{
             botonGirar.isEnabled=false
             empezarContador(contador, botonGirar)
         }
-        //Logica botón de volumen
+        //Logica botón de volumen para cambiar icono
         botonMusica.setOnClickListener{
             if(botonMusica.isChecked){
                 botonMusica.setCompoundDrawablesWithIntrinsicBounds(null, resources.getDrawable(R.drawable.volume_off_fill0_wght400_grad0_opsz24), null, null)
@@ -48,28 +49,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Funcion que sirve de contador de 3 segundos para
+    //mostrar el texto en pantalla y luego hacer girar la botella
     private fun empezarContador(contador: TextView, botonGirar: ImageView) {
         object : CountDownTimer(4000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                // Display the countdown in seconds in the TextView
+                // Muestra el texto que hace la cuenta regresiva
                 contador.visibility = View.VISIBLE
                 contador.text = "${(millisUntilFinished / 1000)}"
             }
 
             override fun onFinish() {
+                //Hace que el texto en pantalla desaparezca cuando llega a 0 la cuenta
                 contador.visibility = View.GONE
+                //Vuelve a activar el boton de girar
                 botonGirar.isEnabled = true
-                // Execute your code here when the countdown ends
+                // Ejecutar codigo cuando el contador termine
                 //funcionesNecesarias()
             }
         }.start()
     }
+
+    //Funcion que reproduce la musica
     private fun tocarMusica(musicaFondo: MediaPlayer) {
         if (!musicaFondo.isPlaying) {
             musicaFondo.isLooping = true // Set looping to true for continuous playback
             musicaFondo.start() // Start playing the music
         }
     }
+    //Funcion que detiene la musica
     private fun pararMusica(musicaFondo: MediaPlayer) {
         if (musicaFondo.isPlaying) {
             musicaFondo.pause() // Pause the music
