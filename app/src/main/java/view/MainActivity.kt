799f257cdb8a2e0707c8d2c9d0e1c72e0d26e1e2
Alejandro <alejorm44@gmail.com>
2023-11-15 22:12:35 -0500

    Agregar carga de retos.

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.ToggleButton
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.bottlegame.R
 
+data class Retos(val Reto: String)
 
 class MainActivity : AppCompatActivity() {
+
+    fun cargarDatosDB(): MutableList<Retos> {
+        return mutableListOf<Retos>(Retos(Reto = "Bailar Salsa"),Retos(Reto = "Comprar Casa"))
+    }
+
+    fun seleccionarReto(data: MutableList<Retos>): Retos {
+        return data.random()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
@@ -23,17 +31,20 @@ class MainActivity : AppCompatActivity() {
         Thread.sleep(5000)
         screenSplash.setKeepOnScreenCondition{false}
 
+        val datos = cargarDatosDB()
+
         //Listeners y asignaciones
         val botonGirar: ImageView = findViewById(R.id.botonGirar)
         val botonMusica: ToggleButton = findViewById(R.id.musica)
         val contador: TextView = findViewById(R.id.cuenta)
+        val retosDPantalla : TextView = findViewById(R.id.textoReto)
         val musicaFondo = MediaPlayer.create(this, R.raw.portal_radio_loop)
         musicaFondo.start()
 
         //Listener boton de girar
         botonGirar.setOnClickListener{
             botonGirar.isEnabled=false
-            empezarContador(contador, botonGirar)
+            empezarContador(contador,retosDPantalla, botonGirar) { seleccionarReto(datos) }
         }
         //Logica botón de volumen para cambiar icono
         botonMusica.setOnClickListener{
@@ -49,9 +60,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+
     //Funcion que sirve de contador de 3 segundos para
     //mostrar el texto en pantalla y luego hacer girar la botella
-    private fun empezarContador(contador: TextView, botonGirar: ImageView) {
+    private fun empezarContador(
+        contador: TextView,
+        retosDPantalla: TextView,
+        botonGirar: ImageView,
+        retoLlamada: () -> Retos
+    ) {
         object : CountDownTimer(4000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 // Muestra el texto que hace la cuenta regresiva
@@ -61,11 +79,24 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 //Hace que el texto en pantalla desaparezca cuando llega a 0 la cuenta
-                contador.visibility = View.GONE
-                //Vuelve a activar el boton de girar
                 botonGirar.isEnabled = true
+                contador.visibility = View.GONE
+                retoContador(retosDPantalla, retoFinal = retoLlamada())
                 // Ejecutar codigo cuando el contador termine
                 //funcionesNecesarias()
+            }
+        }.start()
+    }
+
+    fun retoContador(retosDPantalla: TextView, retoFinal: Retos) {
+        object : CountDownTimer(4000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                retosDPantalla.visibility = View.VISIBLE
+                retosDPantalla.text = retoFinal.Reto
+            }
+
+            override fun onFinish() {
+                retosDPantalla.visibility = View.GONE
             }
         }.start()
     }
@@ -83,5 +114,6 @@ class MainActivity : AppCompatActivity() {
             musicaFondo.pause() // Pause the music
         }
     }
+
 
 }
